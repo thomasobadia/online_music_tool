f2a67a7e1a364313c2a643e5d6533c1e1feb089c
thomasobadia <thomas.obadia@hetic.net>
2018-04-26 16:10:11 +0200

    express serve static files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = require('express')(),
 app.get('/', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
+app.use(express.static('public'));
 
 io.sockets.on('connection', function (socket, pseudo) {
 	// Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
