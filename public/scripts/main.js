6bccd8a302dc64ae2f01f7b0d029d40567ba4ba6
ingalou <ingalprudentecornier@hotmail.fr>
2018-04-30 13:37:19 +0200

    :beers:

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,28 @@
 const $keyBoard = document.querySelector(".keyBoard")
-const $white = $keyBoard.querySelectorAll(".white")
+const $whites = $keyBoard.querySelectorAll(".white")
+
+const pianoSound = new Audio("../../public/sounds/pianoSound.wav")
+
 let whiteLeft = 0
 
 
 
 const createKeyboard = ()=>{
-    for (let i = 0; i < $white.length; i++ ){
+    for (let i = 0; i < $whites.length; i++ ){
         console.log(whiteLeft)
-        $white[i].style.height = $keyBoard.offsetHeight + "px"
-        $white[i].style.width = $keyBoard.offsetWidth/8 + "px"
-        $white[i].style.left = whiteLeft + "px"
-        whiteLeft += parseInt($white[i].style.width, 10) + 4 
+        $whites[i].style.height = $keyBoard.offsetHeight + "px"
+        $whites[i].style.width = $keyBoard.offsetWidth/8 + "px"
+        $whites[i].style.left = whiteLeft + "px"
+        whiteLeft += parseInt($whites[i].style.width, 10) + 4 
     }
 }
+
+$whites.forEach($white => {
+    $white.addEventListener("click", ()=>{
+        pianoSound.currentTime = 0
+        pianoSound.play()
+    })
+})
 
 createKeyboard()
 
