d7c8a907775dafead50789a4c19cef9073ced516
thomasobadia <thomas.obadia@hetic.net>
2018-06-04 09:33:12 +0200

    :sparkles: Improving responsive on homepage

--- scripts/room.js
+++ scripts/room.js
@@ -1,33 +1,36 @@
 var socket = io.connect('http://51.38.176.227:8080')
 const button = document.querySelector('#button')
 const $validate = document.querySelector('#validate')
+const  removeElement = (id) => {
+    var elem = document.getElementById(id);
+    return elem.parentNode.removeChild(elem);
+}
 
 socket.emit('room', roomId);
 socket.emit('players', players);
 
 if (Modernizr.touchevents) {
 	socket.emit('newTouchDevice', 'touchDevice')
-
+	var pseudo = prompt('Enter your pseudo');
+	if(!pseudo){
+		pseudo = 'Anonymous'
+	}
+	socket.emit('newPseudo',{ name: pseudo, room: roomId });
 } else {
 	socket.emit('newComputer', 'computer')
 }
-const updatePseudo = () => {
-	var pseudo = document.querySelector('#pseudo').value
-	console.log('hello')
-	socket.emit('newPseudo', pseudo);
-	console.log(pseudo)
-}
-button.addEventListener('mousedown', () => {
-	socket.emit('sound', 'sound');
-	console.log('hello');
-})
-socket.on('newSound', function () {
-	console.log('yeah');
-})
-socket.on('hello', function () {
-	console.log('hello');
-})
 
+socket.on('addPlayer',  (pseudo) => {
+	if (!Modernizr.touchevents){
+		var div = document.createElement('div')
+		div.setAttribute("id", pseudo);
+		div.innerHTML = pseudo;
+		document.body.appendChild(div)
+}})
+socket.on('removePlayer',  (pseudo) => {
+	if (!Modernizr.touchevents){
+		removeElement(pseudo)
+}})
 socket.on('redirect', function (destination) {
 	window.location.href = destination;
 });
