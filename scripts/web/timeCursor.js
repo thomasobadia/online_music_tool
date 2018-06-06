d79b9fb28a850cda75e05a1385c9d27093d61728
ingalou <ingalprudentecornier@hotmail.fr>
2018-06-06 16:15:59 +0200

    :beer:

--- scripts/web/timeCursor.js
+++ scripts/web/timeCursor.js
@@ -1,12 +1,27 @@
+const $trackContent = document.querySelector(".room__content__tracks__track__content")
+
 const $timeCursor = document.querySelector(".time_cursor")
 const $play = document.querySelector(".room__footer__parameters__audio_controls__play")
 const $record = document.querySelector(".room__footer__parameters__audio_controls__record")
 const $pause = document.querySelector(".room__footer__parameters__audio_controls__pause")
+const $volume = document.querySelector(".tracks__track__volume")
 
+let trackContentWidth = $trackContent.offsetWidth
+let timeCursorDistance = trackContentWidth / 100
 let timeCursorPosition = 0
 let isPause = true
 
+console.log(timeCursorDistance)
 
+window.addEventListener("resize", ()=>{
+    trackContentWidth = $trackContent.offsetWidth
+    timeCursorDistance = trackContentWidth / 100
+    console.log(timeCursorDistance)
+})
+
+
+let timeCursorLimit = $volume.offsetLeft - $timeCursor.offsetLeft
+console.log(timeCursorLimit)
 const loop = ()=>{
     window.requestAnimationFrame(loop)
     mooveTimeCursor()
@@ -14,11 +29,11 @@ const loop = ()=>{
 
 const mooveTimeCursor = ()=>{
     if(isPause === -1){
-        timeCursorPosition+= 1
+        timeCursorPosition+= timeCursorDistance
+        console.log(timeCursorDistance)
     }
     $timeCursor.style.transform = "translateX("+ timeCursorPosition + "px"
-    console.log(timeCursorPosition)
-    if(timeCursorPosition > 674){
+    if(timeCursorPosition > trackContentWidth){
         timeCursorPosition = 0
     }
 
