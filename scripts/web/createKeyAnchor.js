512c9e25248f621292e502e49769906ce1212df6
ingalou <ingalprudentecornier@hotmail.fr>
2018-06-09 18:15:42 +0200

    :sparkles: key anchor added

---
+++ scripts/web/createKeyAnchor.js
@@ -0,0 +1,73 @@
+
+
+const $subTracks = document.querySelectorAll(".sub_track")
+const anchors = []
+let whichTrack = 0
+
+
+console.log($subTracks[0])
+
+//when a key is pressed
+document.addEventListener("keydown", (event)=>{
+    if (event.keyCode===65){
+        if(!event.repeat){
+        //creation of the div
+        const newAnchorDiv = document.createElement('div')
+        newAnchorDiv.setAttribute("class", 'anchor')
+
+        //giving temporal info to the div
+        newAnchorDiv.setAttribute("data-start", timeCursorPosition)
+
+        //position of the div
+        newAnchorDiv.style.transform = "translateX("+timeCursorPosition+"px)"
+
+        //the div is not finished yet
+        newAnchorDiv.setAttribute("data-isfinished", 0)
+
+        //which instrument should we play
+        newAnchorDiv.setAttribute("data-instrument", "piano")
+
+        //on which track the anchor should be placed
+        $subTracks[whichTrack].appendChild(newAnchorDiv)
+        
+        //push the anchor in the array to save it
+        anchors.push(newAnchorDiv)
+        } 
+
+       
+          
+    }
+})
+
+//when a key is released
+document.addEventListener("keyup", ()=>{
+    //a key
+    if (event.keyCode===65){    
+        anchors.forEach(anchor => {
+            let width
+            //if the key is not drawn yet
+            if(!anchor.dataset.isFinished){
+                console.log(timeCursorPosition)
+                console.log(anchor.dataset.start)
+
+                //we record when where we release the key
+                anchor.dataset.stop = timeCursorPosition
+
+                //handle if we release the key after the end of the track
+                if(anchor.dataset.stop > anchor.dataset.start){
+                     width = anchor.dataset.stop - anchor.dataset.start
+                    console.log("bitch")
+                }
+                else{
+                     width = trackContentWidth - anchor.dataset.start 
+                }
+                console.log(width)
+                anchor.style.width = width +"px"
+                anchor.dataset.isFinished = 1
+            }
+        })
+    }
+})
+
+
+
--- scripts/web/timeCursor.js
+++ scripts/web/timeCursor.js
@@ -1,12 +1,29 @@
+const $trackContent = document.querySelector(".room__content__tracks__track__content")
+
 const $timeCursor = document.querySelector(".time_cursor")
 const $play = document.querySelector(".room__footer__parameters__audio_controls__play")
 const $record = document.querySelector(".room__footer__parameters__audio_controls__record")
 const $pause = document.querySelector(".room__footer__parameters__audio_controls__pause")
+const $volume = document.querySelector(".tracks__track__volume")
 
+let trackContentWidth = $trackContent.offsetWidth
+//speed of the timeCursor
+let timeCursorBPM = 500
+let timeCursorDistance = trackContentWidth / timeCursorBPM
 let timeCursorPosition = 0
 let isPause = true
 
+console.log(timeCursorDistance)
 
+window.addEventListener("resize", ()=>{
+    trackContentWidth = $trackContent.offsetWidth
+    timeCursorDistance = trackContentWidth / 100
+    console.log(timeCursorDistance)
+})
+
+
+let timeCursorLimit = $volume.offsetLeft - $timeCursor.offsetLeft
+console.log(timeCursorLimit)
 const loop = ()=>{
     window.requestAnimationFrame(loop)
     mooveTimeCursor()
@@ -14,12 +31,12 @@ const loop = ()=>{
 
 const mooveTimeCursor = ()=>{
     if(isPause === -1){
-        timeCursorPosition+= 1
+        timeCursorPosition+= timeCursorDistance
     }
     $timeCursor.style.transform = "translateX("+ timeCursorPosition + "px"
-    console.log(timeCursorPosition)
-    if(timeCursorPosition > 674){
+    if(timeCursorPosition > trackContentWidth){
         timeCursorPosition = 0
+        isPause = 1
     }
 
 }
@@ -42,5 +59,7 @@ $pause.addEventListener('mouseup', (event)=>{
     }
     isPause = 1
 })
+
+
 
 
