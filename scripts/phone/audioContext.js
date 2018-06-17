74d10d55b977f4a97b3bfff2d6fb840669671c57
ingalou <ingalprudentecornier@hotmail.fr>
2018-06-17 20:03:00 +0200

    :wrench: removing ";'

--- scripts/phone/audioContext.js
+++ scripts/phone/audioContext.js
@@ -4,9 +4,9 @@
 
 if(typeof AudioContext != "undefined" || typeof webkitAudioContext != "undefined") {
     var resumeAudio = function() {
-       if(typeof g_WebAudioContext == "undefined" || g_WebAudioContext == null) return;
-       if(g_WebAudioContext.state == "suspended") g_WebAudioContext.resume();
-       document.removeEventListener("click", resumeAudio);
-    };
-    document.addEventListener("click", resumeAudio);
+       if(typeof g_WebAudioContext == "undefined" || g_WebAudioContext == null) return
+       if(g_WebAudioContext.state == "suspended") g_WebAudioContext.resume()
+       document.removeEventListener("click", resumeAudio)
+    }
+    document.addEventListener("click", resumeAudio)
  }
