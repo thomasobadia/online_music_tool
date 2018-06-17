144f4588359743c828a10ffe640dc19fc9f9d92d
thomasobadia <thomas.obadia@hetic.net>
2018-06-17 23:14:11 +0200

    :wrench: Cleaning

--- scripts/web/volume.js
+++ scripts/web/volume.js
@@ -1,3 +1,7 @@
+/**
+ * UPDATING SOUND VOLUME
+ */
+
 const $volumeSlider = document.querySelector('.room__footer__parameters__volume__slider')
 let volumeValue = $volumeSlider.value
 
@@ -5,16 +9,6 @@ const volume = new Tone.Volume()
 volume.mute = true
 
 
-var sampler = new Tone.Sampler({
-    // "C3" : "../assets/sounds/pianoC3.wav",
-    "C3" : "../assets/sounds/test.wav",
-
-}, function(){
-    //sampler will repitch the closest sample
-    sampler.chain(volume, Tone.Master)
-})
-
-console.log($volumeSlider)
 $volumeSlider.addEventListener('mouseup', ()=>{
     volumeValue = $volumeSlider.value
     volume.volume = - volumeValue
