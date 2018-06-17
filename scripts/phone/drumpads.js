39408074fa2f4989def61227b1974a267b7de17b
ingalou <ingalprudentecornier@hotmail.fr>
2018-06-17 15:35:26 +0200

    :sparkles: color change when trigger drumpad key

--- scripts/phone/drumpads.js
+++ scripts/phone/drumpads.js
@@ -6,11 +6,24 @@
 const $drumpadKeyboard = document.querySelector(".drumpad_keyboard")
 const $drumpadKeys = document.querySelectorAll(".drumpad_keys")
 
-//the last one is the name of the folder
-const allDrumpads = [drumpadTest]
-let currentDrumpad = allDrumpads[0]
 
+//what happen when you hit the drumpad's keys
 for(let i = 0; i< $drumpadKeys.length; i++){
-    $drumpadKeys[i].addEventListener("touchstart", ()=>{ socket.emit('drumpad_pressed', "C" + i)})
-    $drumpadKeys[i].addEventListener("touchend", ()=>{ socket.emit('drumpad_released', "C" + i)})
+    $drumpadKeys[i].addEventListener("touchstart", ()=>{ 
+        
+        //send signal to server to trigger the key's attack
+        socket.emit('drumpad_pressed', "C" + i)
+        
+        //changing the color of the text when the key is active
+        $drumpadKeys[i].children[0].style.color = "blue"
+    
+    })
+    $drumpadKeys[i].addEventListener("touchend", ()=>{ 
+        
+        //send signal to server to trigger the key's release
+        socket.emit('drumpad_released', "C" + i)
+        
+        //returnin the base color the key is no longer active
+        $drumpadKeys[i].children[0].style.color = "#535151"
+    })
 }
