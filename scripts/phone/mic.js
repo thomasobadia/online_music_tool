2e4d62aea6c06aacfa8a4a8cd1d8f75799e49ba9
ingalou <ingalprudentecornier@hotmail.fr>
2018-06-17 19:48:36 +0200

    :wrench: cleaning the code

--- scripts/phone/mic.js
+++ scripts/phone/mic.js
@@ -1,29 +1,27 @@
 /***********************************/
  /*Sample the mic*/
 /***********************************/
-
-
-
-
 const $sampleRecord = document.querySelector(".primary_button_sampler0")
 const $sampleStopRecord = document.querySelector(".primary_button_sampler1")
 
 const $samplerInstructions = document.querySelector(".sampler_instruction")
 
+
 let isRecordingMic = 0
 
-
+//send signal to server to trigger the recording of the computer's microphone
 $sampleRecord.addEventListener("click",function() {
   socket.emit('start_recording', 0)
   $sampleRecord.style.backgroundColor = "red"
 })
 
-
+//send signal to server to stop the recording of the computer's microphone
 $sampleStopRecord.addEventListener("click", function() {
   socket.emit('stop_recording', 0)
   $sampleRecord.style.backgroundColor = "rgba(43, 44, 46, 0.9)"
 })
 
+//display the instructions of the sampler
 $samplerInstructions.addEventListener("click", ()=>{
   window.alert("Press record on your phone and record your voice with mic of the computer then press stop to record. You can now play your voice as an instrument")
 })
